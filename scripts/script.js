1328bc9b5a3a3e6c1861c260e897adb922d7178c
gabriela_h_dias@estudante.sesisenai.org.br <gabriela_h_@estudante.sesisenai.org.br>
2026-04-06 11:54:32 -0300

    add

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,8 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     let email = document.getElementById("email").value;
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
-}
+
+    if(nome.length < 3 ) return alert("Nome Inválido");
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+});
