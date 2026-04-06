9756a9fba4dc4ba670cfcf7df4c7eac34f224df3
gabriela_h_dias@estudante.sesisenai.org.br <gabriela_h_@estudante.sesisenai.org.br>
2026-04-06 12:35:49 -0300

    add

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
 
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
 
