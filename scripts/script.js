7a5dc795f1300e2652ba94a5948d8de58da96b95
gabriela_h_dias <gabriela_h_dias@estudante.sesisenai.org.br>
2026-04-08 12:10:50 -0300

    add

--- scripts/script.js
+++ scripts/script.js
@@ -18,8 +18,8 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     if(nome.length < 3 ) return alert("Nome Inválido");
     if(idade.value < 18 ) return alert("Idade Inválida");
     if(telefone.lenght < 8 ) return alert("Telefone Inválido");
-    if(motivo.lenght < 10 ) return alert(Caracteres Insuficientes);
-    if()
+    if(motivo.lenght < 10 ) return alert("Caracteres Insuficientes");
+    if(moradia = apartamento) 
 
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
 });
